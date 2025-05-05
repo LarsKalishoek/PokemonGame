7c64768d4901ea28a811cfc795776046b49e9350
Lars Kalishoek <99069312@mydavinci.nl>
2025-05-05 22:45:33 +0200

    animation added for walking :3

--- Person.java
+++ Person.java
@@ -1,27 +1,89 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Person here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * A Person that can walk with animation using WASD keys.
  */
 public class Person extends Actor
 {
-    /**
-     * Act - do whatever the Person wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act()
-    {
-        // Add your action code here.
-        if(Greenfoot.isKeyDown("w"))
-        setLocation(getX(), getY() -2);
-        if(Greenfoot.isKeyDown("a"))
-        setLocation(getX() -2, getY());
-        if(Greenfoot.isKeyDown("s"))
-        setLocation(getX(), getY() +2);
-        if(Greenfoot.isKeyDown("d"))
-        setLocation(getX() +2, getY());
+    private GreenfootImage[] walkLeftImages;
+    private GreenfootImage[] walkRightImages;
+    private int imageIndex = 0;
+    private int animationCounter = 0;
+    private String lastDirection = "right";
+    private GreenfootImage idleLeft;
+    private GreenfootImage idleRight;
+    
+    public Person() {
+        walkLeftImages = new GreenfootImage[3];
+        walkRightImages = new GreenfootImage[3];
+        idleLeft = new GreenfootImage("boyWalkingAni/leftIdle.png");
+        idleLeft.scale(50,50);
+        
+        idleRight = new GreenfootImage(idleLeft);
+        idleRight.mirrorHorizontally();
+        for (int i = 0; i < 3; i++) {
+            walkLeftImages[i] = new GreenfootImage("boyWalkingAni/walkLeft" + (i + 1) + ".png");
+            walkLeftImages[i].scale(50 ,50);
+            
+            walkRightImages[i] = new GreenfootImage(walkLeftImages[i]);
+            walkRightImages[i].mirrorHorizontally();
+        }
+
+        setImage(walkRightImages[0]);
+    }
+
+    public void act() {
+        handleMovement();
+    }
+
+    private void handleMovement() {
+        boolean isMoving = false;
+
+        if (Greenfoot.isKeyDown("w")) {
+            setLocation(getX(), getY() - 2);
+            isMoving = true;
+        }
+        if (Greenfoot.isKeyDown("a")) {
+            setLocation(getX() - 2, getY());
+            lastDirection = "left";
+            isMoving = true;
+        }
+        if (Greenfoot.isKeyDown("s")) {
+            setLocation(getX(), getY() + 2);
+            isMoving = true;
+        }
+        if (Greenfoot.isKeyDown("d")) {
+            setLocation(getX() + 2, getY());
+            lastDirection = "right";
+            isMoving = true;
+        }
+
+        if (isMoving) {
+            animateWalk();
+        } else {
+            animationCounter = 0;
+            imageIndex = 0;
+            if (lastDirection.equals("right")) {
+                setImage(idleRight);
+            } else {
+                setImage(idleLeft);
+            }
+        }
+    }
+
+    private void animateWalk() {
+        animationCounter++;
+    
+        if (animationCounter >= 5) { // Adjust 5 to control animation speed (lower = faster)
+            animationCounter = 0; // Reset the counter every cycle
+    
+            imageIndex = (imageIndex + 1) % walkLeftImages.length;
+    
+            if (lastDirection.equals("right")) {
+                setImage(walkRightImages[imageIndex]);
+            } else {
+                setImage(walkLeftImages[imageIndex]);
+            }
+        }
     }
 }
--- girlBedroom.java
+++ girlBedroom.java
@@ -1,7 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class girlBedroom here.
+ * Write a description of class boyHouse here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
@@ -10,12 +10,17 @@ public class girlBedroom extends World
 {
 
     /**
-     * Constructor for objects of class girlBedroom.
+     * Constructor for objects of class boyHouse.
      * 
      */
     public girlBedroom()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        
+        GreenfootImage bg = new GreenfootImage("girlBedroom.png");
+        bg.scale(getWidth(), getHeight()); // schaal tot wereldgrootte
+        setBackground(bg);
+        addObject(new Person(), getWidth()/2, getHeight()/2);
     }
 }
